7c75d0521deb9f3594266b15d3723a9838547c70
William Mitchell <wemitchell3@gmail.com>
2019-03-19 15:04:36 -0500

    Uploading lighting exercies

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,7 +5,36 @@ outEl.innerHTML = "<h1>Active Businesses</h1>"
 
 let zipCodeProperty = "addressZipCode"
 
-businesses.forEach(business => {
+// businesses.forEach(business => {
+//   outEl.innerHTML += `
+//     <h2>${business.companyName}</h2>
+//     <section>
+//       ${business.addressFullStreet}
+//     </section>
+//     <section>
+//     ${business.addressCity} ${business["addressStateCode"]} ${business[zipCodeProperty]}
+//     </section>
+//   `
+//   outEl.innerHTML += "<hr/>"
+// });
+
+// Array to contain all the New York businesses
+// const newYorkBusinesses = businesses.filter(business => {
+//     console.log(business.addressStateCode)
+//     let inNewYork = false
+  
+//     if (business.addressStateCode === "NY") {
+//         inNewYork = true
+//     }
+  
+//     return inNewYork
+//   })
+
+const newYorkBusinesses = businesses.filter(business =>
+    business.companyIndustry === "Manufacturing")
+    
+
+  newYorkBusinesses.forEach(business => {
   outEl.innerHTML += `
     <h2>${business.companyName}</h2>
     <section>
